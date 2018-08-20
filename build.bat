6d3f395b33e965a57ee87b9d7aade6bf07f0ad19
Ricky C <kf6kjg@users.noreply.github.com>
2018-08-19 22:34:28 -0700

    Conan dependencies and build system updates
    Now compiles in VSCode in Windows, and should compile on AppVeyor with the deps coming from Conan.

--- build.bat
+++ build.bat
@@ -1,13 +1,18 @@
-@ECHO OFF
+IF NOT DEFINED 1 (
+	SET buildType=Debug
+) ELSE (
+	SET buildType=%1
+)
 
 REM Build
+conan install . -s build_type=%buildType% -s arch=x86_64 -s compiler.runtime=MTd --build=missing
 MKDIR build
 CD build
-cmake ..
+cmake -G "Visual Studio 15 2017 Win64" .. -DCMAKE_BUILD_TYPE=%buildType%
 cmake --build .
 CD ..
 
 REM Prepare package
 MKDIR bin
 COPY whip.cfg.sample bin
-COPY build\whip bin
+COPY build\bin\whip.exe bin
--- build.sh
+++ build.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+buildType="$1"
+if [ "x$1" == "x" ]; then
+	buildType="Debug"
+fi
+
 # Build
+conan install . -s build_type=$buildType -s arch=x86_64 --build=missing
 mkdir build 2>/dev/null
 cd build
-cmake ..
+cmake .. -DCMAKE_BUILD_TYPE=$buildType
 cmake --build .
 cd ..
 
